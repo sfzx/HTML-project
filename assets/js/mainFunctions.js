fd8a8091104b37258a6d3028c40f971dcdb22715
sfzx <sfzx@users.noreply.github.com>
2020-05-23 15:30:12 +0300

    refactoring mainFunction.js

--- assets/js/mainFunctions.js
+++ assets/js/mainFunctions.js
@@ -1,4 +1,4 @@
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -10,16 +10,16 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slide");
+  let i;
+  let slides = document.getElementsByClassName("slide");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+  if (n > slides.length) slideIndex = 1    
+  if (n < 1) slideIndex = slides.length
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "flex";  
   dots[slideIndex-1].className += " active";
